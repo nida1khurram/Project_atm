0789031f9769f0b8a6ded83af3c9c158ef5c543e
nida1khurram <147607279+nida1khurram@users.noreply.github.com>
2024-03-28 09:43:39 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myPin = 1234;
+let myBalance = 10000; //dollar
+console.log("**** Bank Balance Detail ****\n");
+let pinAns = await inquirer.prompt([{
+        name: "pin",
+        message: "please enter your pin code",
+        type: "number",
+    },
+]);
+if (pinAns.pin == myPin) {
+    console.log("***your pin code is correct ***");
+    let operatorAns = await inquirer.prompt([{
+            name: "operation",
+            message: "what do you want please select option",
+            type: "list",
+            choices: ["widthraw", "check balance", "fast cash"],
+        },
+    ]);
+    if (operatorAns.operation === "widthraw") {
+        let amountAns = await inquirer.prompt([{
+                name: "amount",
+                message: " please enter your amount to widthraw ",
+                type: "number",
+            },
+        ]);
+        myBalance -= amountAns.amount;
+        if (amountAns.amount <= myBalance) {
+            console.log("your remaining balance is :", myBalance);
+        }
+        else {
+            console.log("Sorry ! You have insufficient balance");
+        }
+        ;
+    }
+    else if (operatorAns.operation === "check balance") {
+        console.log("***Check Balance:*** \n" + "Your current Balance is:,", myBalance);
+    }
+    else if (operatorAns.operation === "fast cash") {
+        let amountFast = await inquirer.prompt([{
+                name: "fast",
+                message: "please select option",
+                type: "list",
+                choices: [1000, 2000, 5000, 10000, 15000],
+            },
+        ]);
+        myBalance -= amountFast.fast;
+        if (amountFast.fast <= myBalance) {
+            console.log("your remaining balance is :", myBalance);
+        }
+        else {
+            console.log("Sorry ! You have insufficient balance");
+        }
+        ;
+    }
+}
+else {
+    console.log("Invalid pin code");
+}
